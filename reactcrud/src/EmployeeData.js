b8120e5d3dffa277c4d0a00aaf86cc07b83b2eb8
YashTambat <yashtambat20@gmail.com>
2024-03-13 11:18:24 +0530

    initial commit

---
+++ reactcrud/src/EmployeeData.js
@@ -0,0 +1,28 @@
+export const EmployeeData =[
+    {
+        id:1,
+        firstName:"Yash",
+        lastName:"Tambat",
+        age:23
+    },
+    {
+        id:2,
+        firstName:"Lucky",
+        lastName:"Vishwakarma",
+        age:21
+    },
+    {
+        id:3,
+        firstName:"Aryan",
+        lastName:"Singh",
+        age:20
+    },
+    {
+        id:4,
+        firstName:"Atharva",
+        lastName:"Thakre",
+        age:20
+    }
+    
+    
+]
